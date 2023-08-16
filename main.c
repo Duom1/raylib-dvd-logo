4364706635df9c0b4c59c20243e848e3f91d98c2
Your Name <you@example.com>
2023-08-16 08:41:11 +0300

    jee jee

--- main.c
+++ main.c
@@ -4,22 +4,23 @@
 int main() {
   Vector2 screen = {1200, 800},
           cube_position = {(float)screen.x / 2 - 20, (float)screen.y / 2 - 20},
-          cube_size = {100, 100}, cube_velocity = {300.0f, 300.0f};
-  /* Image dvd_image = LoadImage("./linux.png"); */
-  /* Texture2D dvd_logo = LoadTextureFromImage(dvd_image); */
+          cube_size = {100, 200}, cube_velocity = {300.0f, 300.0f};
   float dt;
   char info[500];
   int current_fps, xo = 100, x = xo;
+
   InitWindow(screen.x, screen.y, "title");
+  Image dvd_image = LoadImage("./dvd_logo.png");
+  ImageResize(&dvd_image, cube_size.y, cube_size.x);
+  Texture2D dvd_logo = LoadTextureFromImage(dvd_image);
+  UnloadImage(dvd_image);
   SetTargetFPS(500);
 
   while (!WindowShouldClose()) {
 
     BeginDrawing();
     ClearBackground(WHITE);
-    DrawRectangle(cube_position.x, cube_position.y, cube_size.x, cube_size.y,
-                  RED);
-    /* DrawTexture(dvd_logo, cube_position.x, cube_position.y, RAYWHITE); */
+    DrawTexture(dvd_logo, cube_position.x, cube_position.y, WHITE);
     EndDrawing();
 
     if (IsKeyPressed(KEY_F1))
@@ -30,9 +31,9 @@ int main() {
       SetTargetFPS(500);
 
     dt = GetFrameTime();
-    if (cube_position.y >= screen.y - cube_size.y || cube_position.y <= 0)
+    if (cube_position.y >= screen.y - cube_size.x || cube_position.y <= 0)
       cube_velocity.y *= -1;
-    if (cube_position.x >= screen.x - cube_size.x || cube_position.x <= 0)
+    if (cube_position.x >= screen.x - cube_size.y || cube_position.x <= 0)
       cube_velocity.x *= -1;
     cube_position.x += cube_velocity.x * dt;
     cube_position.y += cube_velocity.y * dt;
@@ -46,8 +47,9 @@ int main() {
               cube_velocity.y, dt);
       SetWindowTitle(info);
     }
-    x++;
+    x += 0.5 * dt;
   }
+  UnloadTexture(dvd_logo);
   CloseWindow();
   return 0;
 }
